9651a8db674e9a3a917bfb0e3eb60d2447336e8c
Zacharie Day <zachdayz@gmail.com>
2020-10-11 23:17:17 -0400

    Basic camera improvements for portrait mode
    Fixed the "zoomed out" feeling by reducing the base highway
    length.
    
    Still needs to test the effects this has on portrait gameplay.

--- NeuroSonic/GamePlay/GameLayer.cs
+++ NeuroSonic/GamePlay/GameLayer.cs
@@ -923,7 +923,7 @@ namespace NeuroSonic.GamePlay
             const float SCALING = 1.05f;
             if (Window.Width > Window.Height)
                 m_highwayView.Viewport = ((int)(Window.Width - Window.Height * SCALING) / 2, (int)(Window.Height * 0.95f - Window.Height * SCALING), (int)(Window.Height * SCALING));
-            else m_highwayView.Viewport = (0, (Window.Height - Window.Width) / 2 - Window.Width / 5, Window.Width);
+            else m_highwayView.Viewport = (0, (Window.Height - Window.Width) / 2 - Window.Width / 7, Window.Width);
             m_highwayView.Update();
 
             {
